a4730d23acf56dbbf0b26fab2b936ddfc5f04cc3
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-04-04 09:49:06 -0300

    commit mauri
    Signed-off-by: MauricioRaulFerreyra <mauriciotauro82@hotmail.com>

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,28 @@
-import React from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Button from "@mui/material/Button";
-import styles from "./Nav.module.css";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-
-import axios from 'axios'
+import React from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Button from '@mui/material/Button'
+import styles from './Nav.module.css'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom'
 import { axiosWithOutToken } from '../../services/axios'
 
-//axios.defaults.baseURL = 'http://localhost:3001/api'
+//axios.defaults.baseURL = 'http://localhost:4001/api'
 
 const Nav = () => {
-  const { isAuthenticated, user, loginWithPopup, logout } = useAuth0();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  const { isAuthenticated, user, loginWithPopup, logout } = useAuth0()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   const handleClickLogout = () => {
-    logout();
-  };
+    logout()
+  }
 
   let auth0User = async () => {
     const userpost = await axiosWithOutToken('/postUser', user, 'post')
@@ -33,20 +31,20 @@ const Nav = () => {
   }
 
   return (
-    <header className="header-container-general">
+    <header className='header-container-general'>
       <div
-        className="navbar-header"
-        style={{ borderBottom: "3px solid #d5e3e6" }}
+        className='navbar-header'
+        style={{ borderBottom: '3px solid #d5e3e6' }}
       >
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "top",
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'top'
           }}
         >
-          <div className="navbar-brand-box" style={{ background: "#FDFEFE" }}>
-            <Link to="/" style={{textDecoration: "none"}}>
+          <div className='navbar-brand-box' style={{ background: '#FDFEFE' }}>
+            <Link to='/' style={{ textDecoration: 'none' }}>
               <span className={styles.containerTitle}>
                 <h2 className={styles.url}>flyXworld</h2>
               </span>
@@ -57,21 +55,21 @@ const Nav = () => {
             {isAuthenticated && auth0User() ? (
               <div>
                 <Button
-                  id="basic-button"
-                  aria-controls={open ? "basic-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
+                  id='basic-button'
+                  aria-controls={open ? 'basic-menu' : undefined}
+                  aria-haspopup='true'
+                  aria-expanded={open ? 'true' : undefined}
                   onClick={handleClick}
                 >
-                  <img className={styles.imgLogin} src={user.picture} alt="" />
+                  <img className={styles.imgLogin} src={user.picture} alt='' />
                 </Button>
                 <Menu
-                  id="basic-menu"
+                  id='basic-menu'
                   anchorEl={anchorEl}
                   open={open}
                   onClose={handleClose}
                   MenuListProps={{
-                    "aria-labelledby": "basic-button",
+                    'aria-labelledby': 'basic-button'
                   }}
                 >
                   <MenuItem onClick={handleClose}>Profile</MenuItem>
@@ -80,7 +78,7 @@ const Nav = () => {
                 </Menu>
               </div>
             ) : (
-              <Button variant="outlined" onClick={() => loginWithPopup()}>
+              <Button variant='outlined' onClick={() => loginWithPopup()}>
                 Log In / Register
               </Button>
             )}
@@ -88,7 +86,7 @@ const Nav = () => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
