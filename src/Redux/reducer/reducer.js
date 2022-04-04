8cb4a780ce6707fa2270400c145497a08660447b
Mauricio Raul Ferreyra <mauriciotauro82@hotmail.com>
2022-04-04 12:37:16 -0300

    implement error 400

--- src/Redux/reducer/reducer.js
+++ src/Redux/reducer/reducer.js
@@ -1,19 +1,27 @@
-import { GET_FLIGHTS_INFO } from "../actions/constants";
+import { GET_FLIGHTS_INFO, FLIGHTS_NO_FOUND } from "../actions/constants";
 
 const initialState = {
   flights: [],
   allFlights: [],
+  modalErr : false
 };
 
 export default function reducer(state = initialState, action) {
+  console.log(action)
   switch (action.type) {
     case GET_FLIGHTS_INFO:
       return {
         ...state,
         flights: action.payload,
-        allFlights: action.payload,
+        allFlights: action.payload
       };
-
+    case FLIGHTS_NO_FOUND:
+      return {
+        ...state,
+        flights: [],
+        allFlights: [],
+        modalErr: action.payload
+      }
     default:
       return state;
   }
