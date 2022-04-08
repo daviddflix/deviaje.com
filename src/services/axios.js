104225aee81f8dbd149afbc51862bda88fa0a9f8
daviddflix <david-972010@hotmail.com>
2022-04-08 16:31:57 -0300

    deploy 2

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://deviaje.herokuapp.com/getFlights";
+axios.defaults.baseURL = "https://deviaje.herokuapp.com";
 
 export const axiosWithOutToken = async (endpoint, data, method = "GET") => {
    if (method === "GET") {
