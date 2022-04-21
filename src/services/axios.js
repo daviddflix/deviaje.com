02b69f09b4479676fd3fc75119b57e60cdf92778
David Pajaro <david-972010@hotmail.com>
2022-04-21 09:33:03 -0300

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:4001";   
+axios.defaults.baseURL = "http://deviaje.kerokuapp.com";   
 //https://deviaje.herokuapp.com
    
 export const axiosWithOutToken = async (endpoint, data, method = "GET") => {
